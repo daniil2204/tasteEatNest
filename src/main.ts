d81be515e87aebf8bce723f2fab468a7874efbf6
Daniil Drahin <dragindaniilmeizu@gmail.com>
2024-06-28 21:05:56 +0300

    cors change

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   );
   app.enableCors({
     credentials: true,
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000', 'https://taste-eat-next.vercel.app'],
   });
   await app.listen(4000);
 }
